e30681c96c52cafa204526fbe94100d581c41d04
Val <bunsthedev@gmail.com>
2025-03-28 20:48:04 -0500

    add components: CodeSnippet, ModernCard

---
+++ apps/portfolio/components/code/snippet.tsx
@@ -0,0 +1,5 @@
+import CodeSnippetComponent from "@/components/ui/code/code-snippet";
+
+export function CodeSnippet() {
+  return <CodeSnippetComponent>npm install -g create-dedevs-app</CodeSnippetComponent>;
+}
